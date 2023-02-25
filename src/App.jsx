2864a13a5104f855174b8638ed3c746c30f51258
BryanPSK <48677467+BryanPSK@users.noreply.github.com>
2023-02-25 11:25:27 +0800

    Initial SideBarNav. to add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
-import { login, logout } from "./globalStates/userState";
+// import { useDispatch, useSelector } from "react-redux";
+import TemporaryDrawer from "./components/SideBar.tsx";
+// import { login, logout } from "./globalStates/userState";
 
 const App = () => {
-    const { userState } = useSelector((state) => state.userState.user);
-    const dispatch = useDispatch(login());
+    // const { userState } = useSelector((state) => state.userState.user);
+    // const dispatch = useDispatch(login());
     //to use the reducer functions, call dispatch(reducerfunction(userObject))
     //EXAMPLE USE: 
     return(
         <div>
+            <TemporaryDrawer>
+
+            </TemporaryDrawer>
         </div>
     );
 }
