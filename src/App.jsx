5b7b498cb235ccd4b541ba9b8a53a017a78572ae
wishdbekuc <emanuel.lcw@gmail.com>
2023-02-25 10:46:24 +0800

    Added global states folder containing userState
    Multi-line description of commit,
    feel free to be detailed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "./globalStates/count";
+
+
 const App = () => {
+    const { userState } = useSelector((state) => state.userState.user);
+    const dispatch = useDispatch(login());
+    //to use the reducer functions, call dispatch(reducerfunction(userObject))
+    //EXAMPLE USE: 
     return(
         <div>Hi</div>
     );
