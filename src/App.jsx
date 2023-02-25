453ec25ae6c1a20779f49e22e6f872dd5f07ca86
BryanPSK <48677467+BryanPSK@users.noreply.github.com>
2023-02-25 12:30:51 +0800

    added SideBarNav. pass pages in as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 // import { useDispatch, useSelector } from "react-redux";
 import TemporaryDrawer from "./components/SideBar.tsx";
+import Card from '@mui/material/Card';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
+
+
+import SideBarNav from "./components/SideBar.tsx";
 // import { login, logout } from "./globalStates/userState";
-
-const App = () => {
-    // const { userState } = useSelector((state) => state.userState.user);
-    // const dispatch = useDispatch(login());
-    //to use the reducer functions, call dispatch(reducerfunction(userObject))
-    //EXAMPLE USE: 
+const test = () => {
     return(
-        <div>
-            <TemporaryDrawer>
-
-            </TemporaryDrawer>
-        </div>
-    );
+        <Typography>
+            page props
+        </Typography>
+    )
 }
+const App = () => {
+  // const { userState } = useSelector((state) => state.userState.user);
+  // const dispatch = useDispatch(login());
+  //to use the reducer functions, call dispatch(reducerfunction(userObject))
+  //EXAMPLE USE:
+  return (
+    
+    <SideBarNav page ={test()}/>
+    
+  );
+};
 
 export default App;
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -11,79 +14,46 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
-type Anchor = 'left' 
+const drawerWidth = 240;
+//Pass in the page as props;
+export default function SideBarNav({page}) {
 
-export default function TemporaryDrawer() {
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
-      ) {
-        return;
-      }
-
-      setState({ ...state, [anchor]: open });
-    };
-
-  const list = (anchor: Anchor) => (
-    <Box
-      // sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-      role="presentation"
-      // onClick={toggleDrawer(anchor, false)}
-      // onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
 
   return (
-    <div>
-      {(['left'] as const).map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <Drawer
-            anchor={anchor}
-            open={true}
-            // onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Divider />
+        <List>
+          {['Home', 'View Claims', 'Create Claim', 'Edit Claim'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton href='/'>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+      >
+          {page}       
+      </Box>
+    </Box>
   );
 }
