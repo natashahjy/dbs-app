4e14a25185acd14609f0550d64bde4087fb90a2f
Natasha Heng <natashahengjy@gmail.com>
2023-02-25 13:30:44 +0800

    fix alightment

--- src/components/ViewClaim.jsx
+++ src/components/ViewClaim.jsx
@@ -75,9 +75,6 @@ function ViewClaim() {
                   key={row.name}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
                   <TableCell align="right">{row.ClaimID}</TableCell>
                   <TableCell align="right">{row.InsuranceID}</TableCell>
                   <TableCell align="right">{row.InsuranceType}</TableCell>
